6679a6a668302c22d8042849c9f031f75933f4d0
Bintaaaa <bijantium.yt.32@gmail.com>
2022-03-30 09:16:28 +0700

    add new schema

--- src/validator/schema.js
+++ src/validator/schema.js
@@ -1,13 +1,13 @@
 const Joi = require("joi");
-
+const currentYear = new Date().getFullYear();
 const AlbumPayloadSchema = Joi.object({
     name: Joi.string().required(),
-    year: Joi.number().required(),
+    year: Joi.number().integer().min(1900).max(currentYear).required(),
 });
 
 const SongPayloadSchema = Joi.object({
     title: Joi.string().required(),
-    year: Joi.number().required(),
+    year: Joi.number().integer().min(1900).max(currentYear).required(),
     genre: Joi.string().required(),
     performer: Joi.string().required(),
     duration: Joi.number(),
